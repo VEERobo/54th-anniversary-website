ac0460f1bb6de2e66b6348b5f691d0e46a48a107
VEERobo <71770036+VEERobo@users.noreply.github.com>
2024-05-10 22:37:45 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,183 @@
+function answer1() {
+    var answer_1 = document.getElementById("answer_1").value;
+    if (answer_1 === "dhamangaon"){
+        window.location.href = "2nd_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function start() {
+    window.location.href = "1st_page.html";
+}
+function answer2() {
+    var answer_2 = document.getElementById("answer_2").value;
+    if (answer_2 === "1981"){
+        window.location.href = "3rd_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer3() {
+    var answer_3 = document.getElementById("answer_3").value;
+    if (answer_3 === "1981"){
+        window.location.href = "4th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer4() {
+    var answer_4 = document.getElementById("answer_4").value;
+    if (answer_4 === "1980"){
+        window.location.href = "5th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer5() {
+    var answer_5 = document.getElementById("answer_5").value;
+    if (answer_5 === "paneer"){
+        window.location.href = "6th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer6() {
+    var answer_6 = document.getElementById("answer_6").value;
+    if (answer_6 === "corn capsicum"){
+        window.location.href = "7th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer7() {
+    var answer_7 = document.getElementById("answer_7").value;
+    if (answer_7 === "reading"){
+        window.location.href = "8th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer8() {
+    var answer_8 = document.getElementById("answer_8").value;
+    if (answer_8 === "solving puzzle"){
+        window.location.href = "9th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer9() {
+    var answer_9 = document.getElementById("answer_9").value;
+    if (answer_9 === "walking"){
+        window.location.href = "10th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer10() {
+    var answer_10 = document.getElementById("answer_10").value;
+    if (answer_10 === "kho kho"){
+        window.location.href = "11th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer11() {
+    var answer_11 = document.getElementById("answer_11").value;
+    if (answer_11 === "dev anand"){
+        window.location.href = "12th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer12() {
+    var answer_12 = document.getElementById("answer_12").value;
+    if (answer_12 === "white"){
+        window.location.href = "13th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer13() {
+    var answer_13 = document.getElementById("answer_13").value;
+    if (answer_13 === "shirt trouser and tie"){
+        window.location.href = "14th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer14() {
+    var answer_14 = document.getElementById("answer_14").value;
+    if (answer_14 === "sari"){
+        window.location.href = "15th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer15() {
+    var answer_15 = document.getElementById("answer_15").value;
+    if (answer_15 === "green tea"){
+        window.location.href = "16th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer16() {
+    var answer_16 = document.getElementById("answer_16").value;
+    if (answer_16 === "coffee"){
+        window.location.href = "17th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer17() {
+    var answer_17 = document.getElementById("answer_17").value;
+    if (answer_17 === "rajbhog"){
+        window.location.href = "18th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer18() {
+    var answer_18 = document.getElementById("answer_18").value;
+    if (answer_18 === "roasted almond"){
+        window.location.href = "19th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer19() {
+    var answer_19 = document.getElementById("answer_19").value;
+    if (answer_19 === "lab"){
+        window.location.href = "20th_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
+function answer20() {
+    var answer_20 = document.getElementById("answer_20").value;
+    if (answer_20 === "winter"){
+        window.location.href = "last_page.html";
+    }
+    else {
+        alert("Incorrect!! Please try again.");
+    }
+};
